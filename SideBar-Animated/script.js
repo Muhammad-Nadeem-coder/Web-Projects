df80a9b650598186e83bf41a5d1e43099289b4bf
Muhammad Nadeem <itstech4me@gmail.com>
2025-07-31 01:37:51 +0500

    Add files via upload

---
+++ SideBar-Animated/script.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("sidebar").style.width= "200px";
+    document.getElementById("main").style.marginLeft= "200px";
+}
+
+function closeNav(){
+    document.getElementById("sidebar").style.width= "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
